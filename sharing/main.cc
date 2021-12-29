31dd499c9fa1125584a6667e165f67d851d11ab0
Ryan Coulter <ryancoulter22@gmail.com>
2021-12-29 09:17:31 -0500

    Thread safe stack

---
+++ sharing/main.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <list>
+#include <algorithm>
+#include <exception>
+#include <memory>
+#include <stack>
+
+std::mutex some_mutex;
+std::list<int> some_list;
+
+void add_to_list(int new_value)
+{
+    std::lock_guard<std::mutex> guard(some_mutex);
+    some_list.push_back(new_value);
+}
+
+bool list_contains(int value_to_find) 
+{
+    std::lock_guard<std::mutex> guard(some_mutex);
+      return std::find(some_list.begin(),some_list.end(),value_to_find) != some_list.end();
+}
+
+struct empty_stack: std::exception
+{
+    const char* what() const throw();
+};
+template<typename T>
+class threadsafe_stack
+{
+private:
+    std::stack<T> data;
+    mutable std::mutex m;
+public:
+    threadsafe_stack(){}
+    threadsafe_stack(const threadsafe_stack& other)
+    {
+        std::lock_guard<std::mutex> lock(other.m);
+        data=other.data;
+    }
+    threadsafe_stack& operator=(const threadsafe_stack&) = delete;
+    void push(T new_value)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        data.push(std::move(new_value));
+    }
+    std::shared_ptr<T> pop()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        if(data.empty()) throw empty_stack();
+        std::shared_ptr<T> const res(std::make_shared<T>(data.top()));
+        data.pop();
+        return res;
+    }
+    void pop(T& value)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        if(data.empty()) throw empty_stack();
+        value=data.top();
+        data.pop();
+    }
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock(m);
+        return data.empty();
+    }
+};
+
+
+int main() 
+{
+
+}
