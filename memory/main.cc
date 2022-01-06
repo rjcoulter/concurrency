4018c14f4848fcada585c2f2b2accce29a667635
Ryan Coulter <ryancoulter22@gmail.com>
2022-01-05 19:32:55 -0500

    Add spinlock mutex

--- memory/main.cc
+++ memory/main.cc
@@ -16,13 +16,40 @@
  *   - can be done with something like a mutex
  *   - can also use atomic operations
  * - If there is no enforced ordering between two accesses to a single memory location from sperate threads, one or both of those accesses is not atomic, and one or both is a write, this is a race condition and will bring undefined behvaior
- *
- *
+ * 
+ *   Atomic Operations
+ * - Atomic Operations are indivisble, ie they can't be half done from any view in the system
+ * - Can find standard atomic types in the <atomic> header
+ *   - All operations on such types will be atomic
+ * - Key use case is as a replacement for an op that would otw use a mutex for synchronization
+ * 
  *
  *
 
  **/
+
+#include <atomic>
+
+class spinlock_mutex
+{
+  std::atomic_flag flag;
+public:
+  spinlock_mutex();
+      flag(ATOMIC_FLAG_INIT)
+  {}
+  void lock()
+  {
+    while(flat.test_and_set(std::memory_order_acquire));
+  }
+  void unlock()
+  {
+    flag.clear(std::memory_order_relase);
+  }
+};
+
 int main() 
 {
-
+  std::atomic_flag f=ATOMIC_FLAG_INIT;
+  f.clear(std::memory_order_release);
+  boox x = f.test_and_set();
 }
